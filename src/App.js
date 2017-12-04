99e35ec0256a883ccf434395f9f9bd4e0b822726
nadinesk <nadine.khattak@gmail.com>
2017-12-04 15:47:04 -0500

    removed additional info

--- src/App.js
+++ src/App.js
@@ -25,11 +25,8 @@ class App extends Component {
   render() {
   
     return (
-      <div className="App">
-       
-          <h1 className="App-title">{process.env.REACT_APP_SECRET_CODE}</h1>
-          <BillsList bills={this.props.bills}/>
-          
+      <div className="App">                 
+          <BillsList bills={this.props.bills}/>          
       </div>
     );
   }
