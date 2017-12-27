6c66d0aa291e05c3f990147b35f30e766f22fe58
nadinesk <nadine.khattak@gmail.com>
2017-12-27 11:56:58 -0500

    fix 100

--- src/actions/bills.js
+++ src/actions/bills.js
@@ -2,6 +2,13 @@ import fetch from 'isomorphic-fetch';
 
 var apiKey = process.env.REACT_APP_SECRET_CODE
 
+let header = new Headers({
+        
+         
+          'X-API-KEY': apiKey,   
+          'Content-Type': 'authorization'
+});
+
 const receivedBills = bills => {
   
   return {
@@ -15,9 +22,7 @@ export function getBills(chamber, type,offset) {
   return function(dispatch){                    
     return fetch(`https://api.propublica.org/congress/v1/115/${chamber}/bills/${type}.json?offset=${offset}`, 
     {    		
-         headers: {        		
-        		'X-API-KEY': apiKey,             
-      		 }
+         headers: header
     },{mode: 'no-cors'})
 	 .then(res =>  res.json())
 	 .then(billsFound => {     	     	
