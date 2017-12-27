38e8b9d572e51dcd8cc111d270d35e1a888f37bb
nadinesk <nadine.khattak@gmail.com>
2017-12-27 09:24:35 -0500

    fixed some styling and numbering bills

--- src/components/TypeLinks.js
+++ src/components/TypeLinks.js
@@ -62,14 +62,15 @@ class TypeLinks extends Component {
 
   render() {
     const mostRecentNum = (this.state.offsetNum == '0') ? '20 most recent' : 
-                              (this.state.offsetNum == '20') ? '20-40 most recent' : 
-                                (this.state.offsetNum == '40') ? '40-60 most recent' : 
-                                  (this.state.offsetNum == '60') ? '60-80 most recent' : 
-                                    '80-100 most recent'
+                              (this.state.offsetNum == '20') ? '21-40 most recent' : 
+                                (this.state.offsetNum == '40') ? '41-60 most recent' : 
+                                  (this.state.offsetNum == '60') ? '61-80 most recent' : 
+                                    '81-100 most recent'
     return (
       
       <Row>      	  
-      <h2>{this.state.clicked.toUpperCase()} - {mostRecentNum} </h2> 
+      <h3 className="text-center">{this.state.clicked.toUpperCase()} </h3> 
+      <h4 className="text-center">{mostRecentNum} </h4>
       	  <ButtonToolbar>
           <DropdownButton title='Bill Type'>
             <MenuItem onClick={this.handleOnClick.bind(this)}>Introduced</MenuItem>
@@ -83,16 +84,16 @@ class TypeLinks extends Component {
        
 
             <Button bsStyle="primary"  className="0" onClick={this.handleOnNumClick.bind(this)}>20 most recent</Button>
-            <Button bsStyle="primary"  className="20" onClick={this.handleOnNumClick.bind(this)}>20-40</Button>
-            <Button bsStyle="primary" className="40" onClick={this.handleOnNumClick.bind(this)}>40-60</Button>
-            <Button bsStyle="primary"    className="60" onClick={this.handleOnNumClick.bind(this)}>60-80</Button>
-            <Button bsStyle="primary"    className="80" onClick={this.handleOnNumClick.bind(this)}>80-100</Button>      
+            <Button bsStyle="primary"  className="20" onClick={this.handleOnNumClick.bind(this)}>21-40</Button>
+            <Button bsStyle="primary" className="40" onClick={this.handleOnNumClick.bind(this)}>41-60</Button>
+            <Button bsStyle="primary"    className="60" onClick={this.handleOnNumClick.bind(this)}>61-80</Button>
+            <Button bsStyle="primary"    className="80" onClick={this.handleOnNumClick.bind(this)}>81-100</Button>      
           
         </ButtonToolbar>
 
       <br />     
       </Row>
-      
+
     );
   }
 }
